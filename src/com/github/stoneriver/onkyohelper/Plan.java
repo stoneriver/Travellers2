68370721808c37cc8b8e902ea913fbb8ee8df5ae
stoneriver <stoneriverau1366@gmail.com>
2016-11-13 20:42:44 +0900

    更新しました。

--- src/com/github/stoneriver/onkyohelper/Plan.java
+++ src/com/github/stoneriver/onkyohelper/Plan.java
@@ -11,7 +11,8 @@
  *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *See the License for the specific language governing permissions and
  *limitations under the License.
- */package com.github.stoneriver.onkyohelper;
+ */
+package com.github.stoneriver.onkyohelper;
 
 import java.io.File;
 import java.io.FileInputStream;
